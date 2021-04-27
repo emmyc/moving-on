d9a6bcea7926385a8b6aa5a6fbda6cdeaab55d70
Darren Zhang <darrenzhang22@gmail.com>
2021-04-27 01:06:17 -0700

    Allow click animation to occur after consecutive clicks

--- src/components/DraggableWrapper.js
+++ src/components/DraggableWrapper.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import anime from 'animejs';
 import Draggable from 'react-draggable';
 import '../styles/DraggableWrapper.scss';
@@ -8,27 +8,34 @@ function DraggableWrapper(props) {
   const timeline = useRef(null);
   const previousBoxLocation = useRef(null); //use previous location to distinguish between click and drag
 
+  useEffect(() => {
+    timeline.current = anime.timeline({   //note: any animation that uses transforms will break the draggable
+      autoplay: false,                    //this is because Draggable uses transform: translate to position the draggable element
+      easing: 'easeInOutSine',            //possible fix: try to have a different animation each time, that is dependent on the position moved
+    }).add({
+      targets: '.draggable-wrapper',
+      backgroundColor: ['inherit', 'rgb(255, 0, 0)'],
+      duration: 50,
+    }).add({
+      targets: '.draggable-wrapper',
+      backgroundColor: ['rgb(255,0,0,1)', 'rgb(0,0,0,0)'],
+      duration: 400,
+    });
+  }, []);
+
   const handleStart = (e) => {
     previousBoxLocation.current = e.target.getBoundingClientRect();
   };
 
   const handleStop = (e) => {
     const currentBoxLocation = e.target.getBoundingClientRect();
-    if (currentBoxLocation.top === previousBoxLocation.current.top && currentBoxLocation.left === previousBoxLocation.current.left) {
+    if (currentBoxLocation.x === previousBoxLocation.current.x && currentBoxLocation.y === previousBoxLocation.current.y) {
       handleClick();
     }
+    previousBoxLocation.current = currentBoxLocation;
   }
 
-  const handleClick = () => { //TODO: animation only happens on 1st click
-    console.log("i was clicked");
-    timeline.current = anime.timeline({
-      autoplay: false,
-      easing: 'easeInOutSine',
-    }).add({
-      targets: '.draggable-wrapper',
-      rotate: 360,
-      duration: 1000,
-    });
+  const handleClick = () => {
     timeline.current.pause();
     timeline.current.seek(0);
     timeline.current.play();
@@ -43,7 +50,6 @@ function DraggableWrapper(props) {
         {props.children}
       </div >
     </Draggable>
-
   );
 };
 export default DraggableWrapper;

2a22b1559a3ad44fae4948c9e0cbdf909135d9a3
Darren Zhang <darrenzhang22@gmail.com>
2021-05-04 23:37:33 -0700

    Add fixed bounds detection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
+import CustomDraggable from './components/CustomDraggable';
 import DraggableWrapper from './components/DraggableWrapper';
 
 function App() {
+  const ref1 = useState(null);
+  const ref2 = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,13 +22,23 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-        <DraggableWrapper>
+        </a> */}
+        <DraggableWrapper bounds={[ref1,ref2]}>
           <p>
             Hello World!<br/>
             I am draggable!
           </p>
         </DraggableWrapper>
+        <div className='bound0' ref={ref1}/>
+
+        <div className='bound1'ref={ref2}/>
+        {/* <CustomDraggable>
+          <p>
+            Hello World!<br/>
+            I am a custom draggable component!
+          </p>
+        </CustomDraggable> */}
+        {/* <div className='dropzone'></div> */}
       </header>
     </div>
   );
--- src/components/DraggableWrapper.js
+++ src/components/DraggableWrapper.js
@@ -6,43 +6,98 @@ import '../styles/DraggableWrapper.scss';
 function DraggableWrapper(props) {
   const nodeRef = React.useRef(null);
   const timeline = useRef(null);
-  const previousBoxLocation = useRef(null); //use previous location to distinguish between click and drag
+  const prevLoc = useRef(null); //use previous location to distinguish between click and drag
+  const duration = 500;
+  const [opened, setOpened] = useState(false);
+  let bounds = [
+    {
+      left: 0,
+      right: 100,
+      top: 400,
+      bottom: 500,
+    },
+    {
+      left: window.innerWidth - 100,
+      right: window.innerWidth,
+      top: 400,
+      bottom: 500,
+    }
+  ];
 
   useEffect(() => {
     timeline.current = anime.timeline({   //note: any animation that uses transforms will break the draggable
       autoplay: false,                    //this is because Draggable uses transform: translate to position the draggable element
       easing: 'easeInOutSine',            //possible fix: try to have a different animation each time, that is dependent on the position moved
-    }).add({
-      targets: '.draggable-wrapper',
-      backgroundColor: ['inherit', 'rgb(255, 0, 0)'],
-      duration: 50,
-    }).add({
-      targets: '.draggable-wrapper',
-      backgroundColor: ['rgb(255,0,0,1)', 'rgb(0,0,0,0)'],
-      duration: 400,
-    });
+    })
+      .add({
+        targets: '.draggable-wrapper',
+        scale: 5,
+        duration: duration,
+      })
   }, []);
 
+  const inDropBox = (currLoc) => {
+    for (let i =0; i < bounds.length; i++) {
+      if (overlaps(bounds[i], currLoc) || overlaps(currLoc, bounds[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const overlaps = (box1, box2) => {  //checks if box1 overlaps box2, note: sometimes it isn't commutative when box1 or box2 is much bigger than the other
+    let xOverlaps, yOverlaps;
+    if ((box1.left >= box2.left && box1.left <= box2.right) || (box1.right >= box2.left && box1.rigth <= box2.right)) {
+      xOverlaps = true;
+    } else {
+      xOverlaps = false;
+    }
+    if ((box1.top >= box2.top && box1.top <= box2.bottom) || (box1.bottom >= box2.top && box1.bottom <= box2.bottom)) {
+      yOverlaps = true;
+    } else {
+      yOverlaps = false;
+    }
+    return xOverlaps && yOverlaps;
+  }
+
   const handleStart = (e) => {
-    previousBoxLocation.current = e.target.getBoundingClientRect();
+    prevLoc.current = e.target.getBoundingClientRect();
   };
 
   const handleStop = (e) => {
-    const currentBoxLocation = e.target.getBoundingClientRect();
-    if (currentBoxLocation.x === previousBoxLocation.current.x && currentBoxLocation.y === previousBoxLocation.current.y) {
-      handleClick();
+    const currLoc = e.target.getBoundingClientRect();
+    if (currLoc.x === prevLoc.current.x && currLoc.y === prevLoc.current.y) {
+      if (!opened) {
+        handleClick();
+        setOpened(true);
+      }
+    } else {
+      if (inDropBox(currLoc)) {
+        console.log("i am in a drop bound");
+        timeline.current.add({
+          targets: '.draggable-wrapper',
+          opacity: [1,0],
+          duration: duration
+        });
+        timeline.current.pause();
+        timeline.current.seek(duration+1);
+        timeline.current.play();
+      }
+      setOpened(false);
     }
-    previousBoxLocation.current = currentBoxLocation;
+    prevLoc.current = currLoc;
   }
 
   const handleClick = () => {
     timeline.current.pause();
     timeline.current.seek(0);
     timeline.current.play();
+    props.click && props.click(); //check for undefined
   };
 
   return (
     <Draggable
+      handle='.draggable-wrapper'
       onStart={handleStart}
       onStop={handleStop}
       nodeRef={nodeRef}>

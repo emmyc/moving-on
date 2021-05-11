3a8c7c194516855d658cb8b1ca176fddd07b52b4
Emmy Cao <emmycao11@gmail.com>
2021-05-11 12:56:32 -0700

    temp refactor change demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,41 +5,40 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jest/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 11,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["jest", "react", "import"],
+  plugins: ['jest', 'react', 'import'],
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   rules: {
-    "react/prop-types": "off",
+    'react/prop-types': 'off',
 
     // Style
     quotes: [
-      "error",
-      "single",
+      'error',
+      'single',
       {
         avoidEscape: true,
       },
     ],
 
     // ensures clean diffs, see https://medium.com/@nikgraf/why-you-should-enforce-dangling-commas-for-multiline-statements-d034c98e36f8
-    "comma-dangle": ["error", "always-multiline"],
+    'comma-dangle': ['error', 'always-multiline'],
 
     // Require all imported dependencies are actually declared in package.json
-    "import/no-extraneous-dependencies": [
-      "error",
+    'import/no-extraneous-dependencies': [
+      'error',
       {
         optionalDependencies: false, // Disallow importing optional dependencies (those shouldn't be in use in the project)
         peerDependencies: false, // Disallow importing peer dependencies (that aren't also direct dependencies)
@@ -47,38 +46,38 @@ module.exports = {
     ],
 
     // Require all imported libraries actually resolve (!!required for import/no-extraneous-dependencies to work!!)
-    "import/no-unresolved": ["error"],
+    'import/no-unresolved': ['error'],
 
     // Require an ordering on all imports
-    "import/order": [
-      "warn",
+    'import/order': [
+      'warn',
       {
-        groups: ["builtin", "external"],
+        groups: ['builtin', 'external'],
         alphabetize: {
-          order: "asc",
+          order: 'asc',
           caseInsensitive: true,
         },
       },
     ],
 
     // Cannot import from the same module twice
-    "no-duplicate-imports": ["error"],
+    'no-duplicate-imports': ['error'],
 
     // Required spacing in property declarations (copied from TSLint, defaults are good)
-    "key-spacing": ["error"],
+    'key-spacing': ['error'],
 
     // Require semicolons
-    semi: ["error", "always"],
+    semi: ['error', 'always'],
 
     // Don't unnecessarily quote properties
-    "quote-props": ["error", "consistent-as-needed"],
+    'quote-props': ['error', 'consistent-as-needed'],
 
     // No multiple empty lines
-    "no-multiple-empty-lines": ["error"],
+    'no-multiple-empty-lines': ['error'],
 
     // Max line lengths
-    "max-len": [
-      "error",
+    'max-len': [
+      'error',
       {
         code: 120,
         ignoreUrls: true, // Most common reason to disable it
@@ -90,15 +89,15 @@ module.exports = {
     ],
 
     // Don't leave log statements littering the premises!
-    "no-console": ["error"],
+    'no-console': ['error'],
 
     // Useless diff results
-    "no-trailing-spaces": ["error"],
+    'no-trailing-spaces': ['error'],
 
     // Must use foo.bar instead of foo['bar'] if possible
-    "dot-notation": ["error"],
+    'dot-notation': ['error'],
 
     // Are you sure | is not a typo for || ?
-    "no-bitwise": ["error"],
+    'no-bitwise': ['error'],
   },
 };

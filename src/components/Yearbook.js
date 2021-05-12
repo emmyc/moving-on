8ae7f17af43da0fd8793bec79b0cbc8d5d75ce8f
Darren Zhang <darrenzhang22@gmail.com>
2021-05-11 17:28:58 -0700

    Make page flip animations look smoother, add next and prev page buttons

--- src/components/Yearbook.js
+++ src/components/Yearbook.js
@@ -1,5 +1,4 @@
-import React, { useRef } from 'react';
-import FlipPage from 'react-flip-page';
+import React, { useState, useRef } from 'react';
 import HTMLFlipBook from 'react-pageflip';
 import '../styles/Yearbook.scss';
 
@@ -10,6 +9,7 @@ const Page = React.forwardRef((props, ref) => {
     </div>
   )
 });
+
 function Yearbook() {
   const bookRef = useRef();
   const teams = [
@@ -84,7 +84,7 @@ function Yearbook() {
       ]
     },
     {
-      title: 'Superlatives',
+      title: 'SUPERLATIVES',
       members: [
         {
           name: "Name",
@@ -119,76 +119,68 @@ function Yearbook() {
       ]
     },
   ]
-  const devs = [
-    {
-      name: "Name",
-      year: 2024,
-      major: 'Major',
-    },
-    {
-      name: "Name",
-      year: 2024,
-      major: 'Major',
-    },
-    {
-      name: "Name",
-      year: 2024,
-      major: 'Major',
-    },
-    {
-      name: "Name",
-      year: 2024,
-      major: 'Major',
-    },
-    {
-      name: "Name",
-      year: 2024,
-      major: 'Major',
-    },
-    {
-      name: "Name",
-      year: 2024,
-      major: 'Major',
-    },
-  ];
-  return (
-    // <div className='yearbook'>
-    <HTMLFlipBook
-      width={400}
-      height={600}
-      // showCover={true}
-      className='yearbook'
-    // ref={bookRef}
-    >
-      {/* <div>page 1</div>
-        <div>page 2</div> */}
-      {teams.map((team) => {
-        return (
-          <Page>
-            <h1>{team.title}</h1>
-            <div className='team-grid'>
-              {devs.map((member) => {
-                return (
-                  <div className='profile'>
-                    <div className='fake-pic' />
-                    {member.name}<br />
-                    {member.year}<br />
-                    {member.major}
-                  </div>
-                );
-              })}
-            </div>
-          </Page>
-        );
-      })}
+  const [pageNum, setPageNum] = useState(0);
 
-      <Page>2nd page</Page>
-      <Page>third page</Page>
-      <Page>fourth page</Page>
-      <Page>fifth page</Page>
-      <Page>sixth page</Page>
-    </HTMLFlipBook>
-    // </div>
+  const handlePageChange = (e) => {
+    setPageNum(e.data);
+  };
+
+  const goNextPage = () => {
+    bookRef.current.pageFlip().flipNext();
+  };
+
+  const goPrevPage = () => {
+    bookRef.current.pageFlip().flipPrev();
+  };
+
+  return (
+    <div className='yearbook-page'>
+      <HTMLFlipBook
+        width={400}
+        height={600}
+        size='stretch'
+        minWidth={315}
+        maxWidth={400}
+        minHeight={400}
+        maxHeight={600}
+        maxShadowOpacity={0.5}
+        showCover={true}
+        mobileScrollSupport={true}
+        onFlip={handlePageChange}
+        className='yearbook'
+        ref={bookRef}
+      >
+        <Page>
+          <h1>Yearbook</h1>
+        </Page>
+        {teams.map((team) => {
+          return (
+            <Page>
+              <h1>{team.title}</h1>
+              <div className='team-grid'>
+                {team.members.map((member) => {
+                  return (
+                    <div className='profile'>
+                      <div className='fake-pic' />
+                      {member.name}<br />
+                      {member.year}<br />
+                      {member.major}
+                    </div>
+                  );
+                })}
+              </div>
+            </Page>
+          );
+        })}
+        <Page>
+          <h1>AUTOGRAPHS</h1>
+        </Page>
+        <Page />
+      </HTMLFlipBook>
+
+      <button className='flip-page-btn' onClick={goPrevPage} style={{ gridColumnStart: 1 }}>Prev Page</button>
+      <button className='flip-page-btn' onClick={goNextPage} style={{ gridColumnStart: 3 }}>Next Page</button>
+    </div>
   );
 };
 export default Yearbook;

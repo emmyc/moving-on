6df07c7da61b846a652e6931cb4a9d59ff93a000
zhangallison <alzhang19@ucla.edu>
2021-05-12 17:36:21 -0700

    page switching logic
    needs content updated and styling

--- src/components/Narrative.js
+++ src/components/Narrative.js
@@ -1,10 +1,55 @@
 import React from 'react';
 
-class Narrative extends React.Component {
+class NarrativeContent extends React.Component {
+
+    // could also pass in page number here and render something different based on page number
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return(
-            <div className="intro-page">
-                <p>narrative page</p>
+            <div className="page-content">
+                <p>{this.props.text}</p>
+            </div>
+        )
+    }
+}
+
+class Narrative extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            pageNum: 1,
+        }
+    }
+    handlePrevClick() {
+        if (this.state.pageNum > 1)
+            this.setState({pageNum: this.state.pageNum - 1});
+    }
+
+    handleNextClick() {
+        if (this.state.pageNum < 3) 
+            this.setState({pageNum: this.state.pageNum + 1});
+    }
+    
+    
+    render() {
+        let content;
+        if (this.state.pageNum <= 1) {
+            content = <NarrativeContent text="page1"/>;
+        }
+        else if (this.state.pageNum === 2) {
+            content = <NarrativeContent text="page2"/>;
+        }
+        else if (this.state.pageNum >= 3) {
+            content = <NarrativeContent text="page3"/>;
+        }
+        return(
+            <div className="narrative-container">
+                <button className="prevButton" onClick={() => this.handlePrevClick()}>prev</button>
+                {content}
+                <button className="nextButton" onClick={() => this.handleNextClick()}>next</button>
             </div>
         )
     }

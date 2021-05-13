bd7302cb478892c5c4af84e94eb41e2639bf4503
Emmy Cao <emmycao11@gmail.com>
2021-05-12 17:43:16 -0700

    initial styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './styles/main.scss';
 import IntroPageWrapper from './components/IntroPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <header>
         <IntroPageWrapper />
       </header>

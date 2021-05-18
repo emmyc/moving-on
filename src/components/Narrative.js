e701c6c5c01e6367ff4688ee32306a54a69f1054
zhangallison <alzhang19@ucla.edu>
2021-05-17 23:56:08 -0700

    last page goes to explore page, basic progress bar
    needs content and styling still

--- src/components/Narrative.js
+++ src/components/Narrative.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from "react-router-dom";  
+import '../styles/Narrative.scss';
 
 class NarrativeContent extends React.Component {
 
@@ -19,6 +21,7 @@ class NarrativeContent extends React.Component {
 class Narrative extends React.Component {
     constructor(props) {
         super(props)
+        this.numPages = 3;
         this.state = {
             pageNum: 1,
         }
@@ -29,28 +32,41 @@ class Narrative extends React.Component {
     }
 
     handleNextClick() {
-        if (this.state.pageNum < 3) 
+        if (this.state.pageNum < this.numPages) 
             this.setState({pageNum: this.state.pageNum + 1});
     }
     
     
     render() {
         let content;
+        let prev;
+        let next;
         if (this.state.pageNum <= 1) {
             content = <NarrativeContent text="page1"/>;
+            next = <button className="nextButton" onClick={() => this.handleNextClick()}>next</button>
         }
         else if (this.state.pageNum === 2) {
+            prev = <button className="prevButton" onClick={() => this.handlePrevClick()}>prev</button>
             content = <NarrativeContent text="page2"/>;
+            next = <button className="nextButton" onClick={() => this.handleNextClick()}>next</button>
         }
         else if (this.state.pageNum >= 3) {
+            prev = <button className="prevButton" onClick={() => this.handlePrevClick()}>prev</button>
             content = <NarrativeContent text="page3"/>;
+            next = <Link to="/explore">
+                <button className="nextButton">next</button>
+            </Link>
         }
         return(
-            <div className="narrative-container">
-                <button className="prevButton" onClick={() => this.handlePrevClick()}>prev</button>
-                {content}
-                <button className="nextButton" onClick={() => this.handleNextClick()}>next</button>
+            <div className="page-container">
+                <div className="narrative-container">
+                    {prev}
+                    {content}
+                    {next}
+                </div>
+                <progress value={(this.state.pageNum / this.numPages)*100} max="100">30%</progress>
             </div>
+            
         )
     }
 }

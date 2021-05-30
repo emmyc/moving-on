c9dc12526864c1dd0ac7ad53288c4a5e8c5b8569
andersonxanna <annaranderson@icloud.com>
2021-05-29 20:54:43 -0700

    adding button for music

--- src/components/Fieldbook.js
+++ src/components/Fieldbook.js
@@ -12,8 +12,11 @@ import sandcrab1 from '../assets/fieldbook/sandcrab1.jpeg';
 import sandcrab2 from '../assets/fieldbook/sandcrab2.jpeg';
 import seal1 from '../assets/fieldbook/seal1.jpeg';
 import seal2 from '../assets/fieldbook/seal2.jpeg';
-
-
+import BlueXmas from '../assets/fieldbook/songs/Blue Christmas.mp3';
+import Sun from '../assets/fieldbook/songs/Here Comes The Sun.mp3';
+import Love from '../assets/fieldbook/songs/How Deep is Your Love.mp3';
+import Space from '../assets/fieldbook/songs/Space Oddity.mp3';
+import Casette from '../assets/fieldbook/cassette.png';
 const Page = React.forwardRef(function Page(props, ref) {
   return (
     <div className='fpage' ref={ref}>
@@ -23,15 +26,64 @@ const Page = React.forwardRef(function Page(props, ref) {
 });
 
 function Fieldbook() {
+  function handleClick() {
+    var pageNum = bookRef.current.getCurrentPageIndex();
+    var songNum = 0;
+    switch(pageNum){
+      case 1:
+      case 2:
+        songNum = 0;
+        break;
+      case 3:
+      case 4:
+        songNum = 1;
+        break;
+      case 5:
+      case 6:
+        songNum = 2;
+        break;
+      case 7:
+      case 8:
+        songNum = 3;
+        break;
+      default:
+        songNum = 0;
+    }
+    if (pageNum != 0 && pageNum != 9) {
+      console.log(songs[songNum]);
+      return (
+      <div style={{ gridColumnStart: 1 }}>
+        <audio src = {songs[songNum]} id = "audio"/>
+        <button type="button"><img src ={Casette} alt='SOMETHING'/></button>
+      </div>
+      );
+    } //className = "songButton"
+  }
   const bookRef = useRef();
-
   const goNextPage = () => {
     bookRef.current.pageFlip().flipNext();
+    handleClick();
   };
 
   const goPrevPage = () => {
     bookRef.current.pageFlip().flipPrev();
+    handleClick();
   };
+
+  const songs = [
+    Sun,
+    Love,
+    Space,
+    BlueXmas,
+  ];
+  /*const colors = [
+    "fieldbook-blue0-overlay",
+    "fieldbook-blue1-overlay",
+    "fieldbook-blue2-overlay",
+    "fieldbook-blue3-overlay",
+    "fieldbook-blue4-overlay",
+  ];*/
+
   const pages = [
     {
       image: cover,

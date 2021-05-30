5c0fda4fbad34160d58ba67e60c0b53613da1919
Darren Zhang <darrenzhang22@gmail.com>
2021-05-30 02:10:30 -0700

    Create Plants component and make background that responds to screen size changes

---
+++ src/components/Plants.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Caption from './Caption';
+
+import '../styles/Plants.scss';
+
+import AllPlantsBackgroundSVG from '../assets/plants/all-plants-background.svg';
+
+function Plants() {
+  const [showingPreview, setShowingPreview] = useState(true);
+  // const [caption, setCaption] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setShowingPreview(false), 500);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <>
+      { showingPreview ?
+        <div className='plant-preview'>
+          you look towards your window to look at your plants basking in the sunlight.
+        </div>
+        :
+        <>
+          <div className='plants-container'>
+            <img className='max-image' src={AllPlantsBackgroundSVG} alt='Background Image of A window with Plants next to it'/>
+          </div>
+          <Caption caption={''} />
+        </>
+      }
+    </>
+  );
+}
+export default Plants;

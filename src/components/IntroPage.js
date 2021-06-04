5bf441a245abb1fe8696a8df86f1f260a719aaf8
Darren Zhang <darrenzhang22@gmail.com>
2021-06-04 15:19:10 -0700

    Add descriptions to appear on hover over the moving and packing links in home page

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/IntroPage.scss';
 import SuitcaseImg from '../assets/icons/suitcase_closed.png';
 
 function IntroPage() {
+  const TEXT_STATES = {
+    NO_HOVER: 0,
+    SCENIC: 1,
+    PACKING: 2,
+  };
+  const [hoverState, setHoverState] = useState(TEXT_STATES.NO_HOVER);
+
   return (
     <div className='intro-page'>
       <div className='intro-pic'>
@@ -16,23 +23,23 @@ function IntroPage() {
 
         <div className='options'>
           <div className='option'>
-            <Link className='narrative-option' to='/narrative'>
+            <Link className='narrative-option' to='/narrative' onMouseEnter={() => setHoverState(TEXT_STATES.SCENIC)} onMouseLeave={() => setHoverState(TEXT_STATES.NO_HOVER)}>
               on why we&apos;re moving
           </Link>
           </div>
           <div className='option'>
-            <Link className='explore-option' to='/explore'>
+            <Link className='explore-option' to='/explore' onMouseEnter={() => setHoverState(TEXT_STATES.PACKING)} onMouseLeave={() => setHoverState(TEXT_STATES.NO_HOVER)}>
               {' '}
             on what we&apos;re packing
           </Link>
           </div>
         </div>
         <div id='option-subtext'>
-          <p className='explore-desc desc'>
+          <p className='explore-desc desc' style={{display: hoverState === TEXT_STATES.PACKING ? 'block' : 'none'}}>
             take me straight to the gameplay babey; interact with the objects
             you have to pack
           </p>
-          <p className='narrative-desc desc'>
+          <p className='narrative-desc desc' style={{display: hoverState === TEXT_STATES.SCENIC ? 'block' : 'none'}}>
             take me through the scenic route; extra narrative content and
             background information on character
           </p>

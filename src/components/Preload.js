5f85e91a850822d8a73802e87dc122795afd722d
Darren Zhang <darrenzhang22@gmail.com>
2021-06-04 18:22:02 -0700

    Split up request to load all images into blocks of 8 instead of reqeusting all images at once

--- src/components/Preload.js
+++ src/components/Preload.js
@@ -7,6 +7,7 @@ const PRELOAD_TIMEOUT_IN_SEC = 10;
 
 function Preload(props) {
   const { images } = props;
+  const REQUEST_BLOCK = 8;
   useEffect(() => {
     // void means the promise's result is being ignored.
     // we do this to avoid an eslint rule: @typescript-eslint/no-floating-promises
@@ -16,18 +17,38 @@ function Preload(props) {
   }, []);
 
   const cacheImages = async (srcArray) => {
-    const promises = await srcArray.map((src) => {
-      return new Promise(function (resolve, reject) {
-        const img = new Image();
-        img.src = src;
-        img.onload = () => resolve(src);
-        img.onerror = () => reject(`Could not load ${src}. Please try again.`);
-      }).catch((error) =>
-        // eslint-disable-next-line no-console
-        console.warn(error),
-      );
-    });
-    await Promise.all(promises);
+    for (let i = 0; i < srcArray.length; i+=REQUEST_BLOCK) {
+      const promises = [];
+      // preload REQUEST_BLOCK number of images
+      for (let j = i; j < srcArray.length && j - i < REQUEST_BLOCK; j++) {
+        promises.push(
+          new Promise(function (resolve, reject) {
+            const img = new Image();
+            img.src = srcArray[j];
+            img.onload = () => resolve(srcArray[j]);
+            img.onerror = () => reject(`Could not load ${srcArray[j]}. Please try again.`);
+          }).catch((error) =>
+            // eslint-disable-next-line no-console
+            console.warn(error),
+          ),
+        );
+      }
+
+      await Promise.all(promises);
+    }
+
+    // const promises = await srcArray.map((src) => {
+    //   return new Promise(function (resolve, reject) {
+    //     const img = new Image();
+    //     img.src = src;
+    //     img.onload = () => resolve(src);
+    //     img.onerror = () => reject(`Could not load ${src}. Please try again.`);
+    //   }).catch((error) =>
+    //     // eslint-disable-next-line no-console
+    //     console.warn(error),
+    //   );
+    // });
+    // await Promise.all(promises);
     props.onPreloaded();
   };
 
